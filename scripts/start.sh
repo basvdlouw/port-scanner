2b8d585c8d73cfec9c8c1cd2a5c5186376e7ddd5
Bas van de Louw <basvdlouw@gmail.com>
2023-06-09 23:29:05 +0200

    add simple selenium project for automating port scanning

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 node /app/public/server/app.js &
 npx --yes ts-node /app/artificial-ports-server/app.ts &
-
+java -jar /app/selenium-portscan.jar &
 # Infinite loop to keep the container running
 while true; do
   sleep 1
---
+++ selenium-portscan/src/main/java/Main.java
@@ -0,0 +1,31 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Main {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        final ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*");
+        final WebDriver driver = new ChromeDriver(chromeOptions);
+
+        try {
+            driver.get("http://localhost:3000/?begin_port=1&end_port=3000&n_scans=1&n_sockets=200&socket_timeout=300&scanning_technique=fetch");
+            driver.findElement(By.id("startPortScanner")).click();
+            final WebDriverWait wait = new WebDriverWait(driver, Duration.ofMinutes(10));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("finished")));
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        } finally {
+            driver.close();
+            driver.quit();
+        }
+    }
+
+}
