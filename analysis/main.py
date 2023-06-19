1107f5be64c8d6ce80ae1f6dec50e6a2b057488b
Bas van de Louw <basvdlouw@gmail.com>
2023-06-19 20:30:18 +0200

    configurable plot

--- analysis/main.py
+++ analysis/main.py
@@ -9,12 +9,13 @@ scan_results_directory = "scan-results"
 
 
 def main():
-    filename = "win_chrome_n_sockets.png"
-    scan_results: list[ScanModel] = get_results("mcr.microsoft.com/windows:20H2-amd64", "fetch", "n_sockets")
+    scan_technique = "xhr"
+    filename = f"win_chrome_n_sockets_{scan_technique}.png"
+    scan_results: list[ScanModel] = get_results("mcr.microsoft.com/windows:20H2-amd64", "SCANNING_TECHNIQUE", scan_technique, "n_sockets")
     plot = get_plot_parallel_sockets(scan_results, filename)
-    plot.show()
+    # plot.show()
 
-def get_results(operating_system: str, scanning_technique: str, sort_key: str):
+def get_results(operating_system: str, filter_property: str, filter_value: str, sort_key: str):
     directory = os.path.abspath(os.path.join(os.path.realpath(__file__), "../..", scan_results_directory))
     results: list[ScanModel] = []
     for item in os.listdir(directory):
@@ -29,7 +30,7 @@ def get_results(operating_system: str, scanning_technique: str, sort_key: str):
                 separator = "="
                 if separator in line:
                     key, value = line.split(separator)
-                    if key == "SCANNING_TECHNIQUE" and value == scanning_technique:
+                    if key == filter_property and value == filter_value:
                         technique_found = True
                         continue
                     if key == "BASE_IMAGE" and value == operating_system:
