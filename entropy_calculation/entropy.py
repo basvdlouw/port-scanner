3b0def1fbc7a7d9d7d6d1aa838a846579dd064b1
Bas van de Louw <basvdlouw@gmail.com>
2023-09-24 16:02:35 +0200

    update

--- entropy_calculation/entropy.py
+++ entropy_calculation/entropy.py
@@ -2,10 +2,12 @@ import numpy as np
 from collections import Counter
 
 # Parameters
-mu = 10  # Mean of the normal distribution for n of identified open ports
-sigma = 5  # Standard deviation of the normal distribution for n of identified open ports
-num_scans = 2000000  # Number of simulated port scans
-exp_parameter = 180  # Decay parameter for the exponential distribution
+mu = 10  # Mean of the normal distribution for scan results
+sigma = 5  # Standard deviation of the normal distribution for scan results
+num_scans = 100000  # Number of simulated scans
+exp_parameter = 200  # Decay parameter for the exponential distribution
+num_ports = 1000  # Number of ports being scanned
+epsilon = 1e-10  # Small non-zero value to replace zero probabilities
 
 # Simulate scans with a normal distribution
 scans = np.random.normal(mu, sigma, num_scans)
@@ -16,35 +18,47 @@ identified_open_ports = []
 # Simulate the identification of open ports in each scan
 for scan in scans:
     if scan > 0:
-        # Round the n of identified open ports to the nearest integer to represent the number of open ports
+        # Round the scan result to the nearest whole number to represent the number of open ports
         x = round(scan)
 
         # Define probabilities for selecting ports using an exponential distribution
-        port_probabilities = np.exp(-np.arange(1001) / exp_parameter)
+        port_probabilities = np.exp(-np.arange(num_ports + 1) / exp_parameter)
 
         # Randomly select open ports based on the defined probabilities
-        selected_ports = np.random.choice(1001, x, replace=False, p=port_probabilities / port_probabilities.sum())
+        selected_ports = np.random.choice(num_ports + 1, x, replace=False, p=port_probabilities / port_probabilities.sum())
 
-        # Add the selected open ports to the list as a set
-        identified_open_ports.append(set(selected_ports))
+        # Add the selected open ports to the list
+        identified_open_ports.append(selected_ports)
     else:
-        # If the scan is 0, there are no open ports identified during the scan
-        identified_open_ports.append(set())
+        # If the scan result is non-positive, there are no open ports in this scan
+        identified_open_ports.append([])
 
-# Calculate the joint probability distribution of unique port combinations
-joint_probability_distribution = Counter(tuple(combination) for combination in identified_open_ports)
+# Calculate the distribution of ports across all sets (excluding the current set)
+total_port_counts = Counter()
+for i in range(len(identified_open_ports)):
+    total_port_counts.update(identified_open_ports[i])
 
-# Initialize the joint entropy
-joint_entropy = 0.0
+# Calculate the entropy for each set of identified open ports based on the distribution of ports across all sets
+entropies = []
+for i in range(len(identified_open_ports)):
+    current_set = set(identified_open_ports[i])
 
-# Calculate the entropy contribution from each unique port combination
-for port_combination, count in joint_probability_distribution.items():
-    # Calculate the probability of the port combination occurring across all scans
-    probability = count / num_scans
+    # Subtract the current set from total_port_counts
+    total_port_counts.subtract(current_set)
 
-    # Calculate the entropy contribution for this combination and accumulate it
-    entropy_contribution = -probability * np.log2(probability)
-    joint_entropy += entropy_contribution
+    total_ports = len(identified_open_ports) - 1  # Excluding the current set
+    port_probabilities = [count / total_ports if count > 0 else epsilon for count in total_port_counts.values()]
 
-# Print the calculated joint entropy
-print("Joint Entropy:", joint_entropy)
+    # Calculate the entropy of the current set relative to the others
+    entropy = -sum(p * np.log2(p) for p in port_probabilities if p > 0)  # Exclude zero probabilities
+    entropies.append(entropy)
+
+# Calculate the average entropy for a set of identified open ports
+average_entropy = sum(entropies) / len(entropies)
+
+# Print the calculated entropies for each set
+for i, entropy in enumerate(entropies):
+    print(f"Entropy for Set {i + 1}: {entropy:.4f}")
+
+# Print the average entropy
+print(f"Average Entropy for Sets of Identified Open Ports: {average_entropy:.4f}")
